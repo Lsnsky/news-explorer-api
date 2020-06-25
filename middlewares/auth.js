03dd39d6fb973132f8f00bf3af5bd7f4ff395422
Igor Lisyanskiy <lsnsky@pm.me>
2020-06-25 18:20:03 +0300

    feat:добавлена мидлвара удаления cookie

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,16 +3,16 @@ const { KEY } = require('../config/index');
 const AuthError = require('../errors/auth-err');
 
 module.exports = (req, res, next) => {
-    const { authorization = '' } = req.headers;
-    if (authorization || authorization.startsWith('Bearer')) {
-        const token = authorization.replace('Bearer ', '');
-        let payload;
-        try {
-            payload = jwt.verify(token, KEY);
-        } catch (err) {
-            return next(new AuthError('Неправильный email или пароль'));
-        }
-        req.user = payload;
+  const { authorization = '' } = req.headers;
+  if (authorization || authorization.startsWith('Bearer')) {
+    const token = authorization.replace('Bearer ', '');
+    let payload;
+    try {
+      payload = jwt.verify(token, KEY);
+    } catch (err) {
+      return next(new AuthError('Неправильный email или пароль'));
     }
-    return next();
+    req.user = payload;
+  }
+  return next();
 };
